f111dc6904779f274328446c17b51d3454c96147
xVoLAnD <xvoland@gmail.com>
2021-05-24 11:18:21 +0100

    javascript support WEBP images

---
+++ app/js/_webp.js
@@ -0,0 +1,11 @@
+function testWebP() {
+    return new Promise(res => {
+        const webP = new Image();
+        webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+        webP.onload = webP.onerror = () => {
+            res(webP.height === 2);
+        };        
+    })
+};
+
+// testWebP().then(hasWebP => console.log(hasWebP));
